19ff84d1201e9dbac3af4d8551d20b42e0b0d80c
JadenStryker <jaden.stryker03@gmail.com>
2023-04-14 14:19:55 -0400

    first content

---
+++ src/components/FrontPage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './FrontPage.css';
+
+const FrontPage = () => {
+  return (
+    <div className="front-page">
+      <div className="content">
+        <h1>Welcome to My Website</h1>
+        <p>
+          This is a demo website created with React and Node.js. Check out our
+          amazing features and services!
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default FrontPage;
